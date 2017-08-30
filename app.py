ec28ef661d2bcba8fc9252ab37223de1345aa1a7
ThiagoLopes <thiagolopes@protonmail.com>
2017-08-30 10:29:54 -0300

    Fix port environ

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def thread_images(name=None, id_t=None):
 @click.option('--debug', default=False)
 def main(debug):
     DEBUG = debug
-    port = int(os.environ.get("port", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=DEBUG)
 
 
